ce7fb0155a4d25a8756b57169ca7979d3c2586c0
Masaki Hara <ackie.h.gmai@gmail.com>
2014-07-13 11:37:00 +0900

    show list syntax sugar.

--- Preterm.hs
+++ Preterm.hs
@@ -10,8 +10,17 @@ data Preterm = PCompound String [Preterm]
                deriving (Show, Eq)
 
 showPreterm :: Preterm -> String
+showPreterm (PCompound "[]" []) = "[]"
+showPreterm (PCompound "." [t0, t1]) = "[" ++ showPretermList t0 t1 ++ "]"
 showPreterm (PCompound sym []) = sym
 showPreterm (PCompound sym args) =
   sym ++ "(" ++ intercalate ", " (map showPreterm args) ++ ")"
 showPreterm (PVariable varname) = varname
 showPreterm PPlaceholder = "_"
+
+showPretermList :: Preterm -> Preterm -> String
+showPretermList t0 (PCompound "[]" []) = showPreterm t0
+showPretermList t0 (PCompound "." [t1, t2]) =
+  showPreterm t0 ++ ", " ++ showPretermList t1 t2
+showPretermList t0 t1 =
+  showPreterm t0 ++ " | " ++ showPreterm t1
