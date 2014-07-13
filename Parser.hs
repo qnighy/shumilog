caab95050c7f6c655eb0c5ca56fad8b666fffaea
Masaki Hara <ackie.h.gmai@gmail.com>
2014-07-13 12:25:17 +0900

    list syntax now working.

--- Parser.hs
+++ Parser.hs
@@ -3,6 +3,7 @@ module Parser (
   OperatorType(XF, YF, XFX, XFY, YFX, FY, FX),
   statementOrEnd
 ) where
+import Data.List ((\\))
 import Text.Parsec
 import Control.Applicative ((<$>),(<$),(<*>),(*>),(<*))
 import Control.Monad.State.Strict
@@ -10,14 +11,15 @@ import Lexer
 import Preterm
 import Terms
 
-operatorIn :: Stream s m Char => Bool -> [String] -> ParsecT s u m String
-operatorIn b syms = try $
+symbolIn :: Stream s m Char => [String] -> [String] -> ParsecT s u m String
+symbolIn [] syms = mzero
+symbolIn (allowedsym:asyms) syms =
+  guard (elem allowedsym syms) *> symbol allowedsym <|> symbolIn asyms syms
+
+operatorIn :: Stream s m Char => [String] -> [String] -> ParsecT s u m String
+operatorIn allowedsyms syms = try $
   do { sym <- atom; guard $ elem sym syms; return sym} <|>
-  do { guard $ not b; _ <- symbol ","; return "," }
--- operatorIn _ [] = mzero
--- operatorIn True (",":symtail) = operatorIn False symtail
--- -- operatorIn b (sym:symtail) = symbol sym <|> operatorIn b symtail
--- operatorIn b (sym:symtail) = symbol sym <|> operatorIn b symtail
+  symbolIn allowedsyms syms
 
 pCompound1L :: String -> Preterm -> Preterm
 pCompound1L sym rhs = PCompound sym [rhs]
@@ -26,43 +28,46 @@ pCompound2 :: Preterm -> String -> Preterm -> Preterm
 pCompound2 lhs sym rhs = PCompound sym [lhs, rhs]
 
 term :: (MonadState Environment m, Stream s m Char) =>
-        Bool -> ParsecT s u m Preterm
-term b = termOps b =<< gets operatorInfo
+        [String] -> ParsecT s u m Preterm
+term asyms = termOps asyms =<< gets operatorInfo
 
 termOps :: (MonadState Environment m, Stream s m Char) =>
-           Bool -> [(Int,OperatorType,[String])] -> ParsecT s u m Preterm
+           [String] -> [(Int,OperatorType,[String])] -> ParsecT s u m Preterm
 termOps _ [] =
-  PCompound <$> atomParen <*> term_list <* symbol ")" <|>
+  PCompound <$> atomParen <*> term_list [",","|"] <* symbol ")" <|>
   PCompound <$> atom <*> return [] <|>
   PPlaceholder <$ symbol "_" <|>
   PVariable <$> variable <|>
-  symbol "(" *> term False <* symbol ")"
-termOps b oprs@((_,XF,oprsym):oprtail) = do
-  trm <- termOps b oprtail
-  syms <- atmost1 $ opEater1 <$> operatorIn b oprsym
+  symbol "(" *> term [",", "|"] <* symbol ")" <|>
+  symbol "[" *>
+    ((return (PCompound "[]" []) <* symbol "]") <|>
+     (term_listsyn [",", "|"] <* symbol "]"))
+termOps asyms oprs@((_,XF,oprsym):oprtail) = do
+  trm <- termOps asyms oprtail
+  syms <- atmost1 $ opEater1 <$> operatorIn asyms oprsym
   return $ fold_eat trm syms
-termOps b oprs@((_,YF,oprsym):oprtail) = do
-  trm <- termOps b oprtail
-  syms <- many $ opEater1 <$> operatorIn b oprsym
+termOps asyms oprs@((_,YF,oprsym):oprtail) = do
+  trm <- termOps asyms oprtail
+  syms <- many $ opEater1 <$> operatorIn asyms oprsym
   return $ fold_eat trm syms
-termOps b oprs@((_,XFX,oprsym):oprtail) = do
-  trm <- termOps b oprtail
-  pCompound2 trm <$> operatorIn b oprsym <*> termOps b oprtail <|>
+termOps asyms oprs@((_,XFX,oprsym):oprtail) = do
+  trm <- termOps asyms oprtail
+  pCompound2 trm <$> operatorIn asyms oprsym <*> termOps asyms oprtail <|>
     return trm
-termOps b oprs@((_,XFY,oprsym):oprtail) = do
-  trm <- termOps b oprtail
-  pCompound2 trm <$> operatorIn b oprsym <*> termOps b oprs <|>
+termOps asyms oprs@((_,XFY,oprsym):oprtail) = do
+  trm <- termOps asyms oprtail
+  pCompound2 trm <$> operatorIn asyms oprsym <*> termOps asyms oprs <|>
     return trm
-termOps b oprs@((_,YFX,oprsym):oprtail) = do
-  trm <- termOps b oprtail
-  syms <- many $ opEater2 <$> operatorIn2 b oprsym oprtail
+termOps asyms oprs@((_,YFX,oprsym):oprtail) = do
+  trm <- termOps asyms oprtail
+  syms <- many $ opEater2 <$> operatorIn2 asyms oprsym oprtail
   return $ fold_eat trm syms
-termOps b oprs@((_,FX,oprsym):oprtail) = do
-  pCompound1L <$> operatorIn b oprsym <*> termOps b oprtail <|>
-    termOps b oprtail
-termOps b oprs@((_,FY,oprsym):oprtail) = do
-  pCompound1L <$> operatorIn b oprsym <*> termOps b oprs <|>
-    termOps b oprtail
+termOps asyms oprs@((_,FX,oprsym):oprtail) = do
+  pCompound1L <$> operatorIn asyms oprsym <*> termOps asyms oprtail <|>
+    termOps asyms oprtail
+termOps asyms oprs@((_,FY,oprsym):oprtail) = do
+  pCompound1L <$> operatorIn asyms oprsym <*> termOps asyms oprs <|>
+    termOps asyms oprtail
 
 atmost1 :: ParsecT s u m a -> ParsecT s u m [a]
 atmost1 p = (:[]) <$> p <|> return []
@@ -78,19 +83,30 @@ fold_eat trm syms_eater = foldl (\t sym_eater -> sym_eater t) trm syms_eater
 
 operatorIn2 ::
   (MonadState Environment m, Stream s m Char) =>
-  Bool -> [String] -> [(Int,OperatorType,[String])] ->
+  [String] -> [String] -> [(Int,OperatorType,[String])] ->
   ParsecT s u m (String, Preterm)
-operatorIn2 b oprsym oprs =
-  (,) <$> operatorIn b oprsym <*> termOps b oprs
+operatorIn2 asyms oprsym oprs =
+  (,) <$> operatorIn asyms oprsym <*> termOps asyms oprs
 
 term_list :: (MonadState Environment m, Stream s m Char) =>
-             ParsecT s u m [Preterm]
-term_list = (:) <$> term True <*> (symbol "," *> term_list <|> return [])
+             [String] -> ParsecT s u m [Preterm]
+term_list asyms =
+  (:) <$> term (asyms \\ [","]) <*> (symbol "," *> term_list asyms <|> return [])
+
+term_listsyn :: (MonadState Environment m, Stream s m Char) =>
+             [String] -> ParsecT s u m Preterm
+term_listsyn asyms = do
+  lst <- term_list (asyms \\ ["|"])
+  (do
+    _ <- symbol "|"
+    trm <- term (asyms \\ [",", "|"])
+    return $ foldr (\x y -> PCompound "." [x, y]) trm lst) <|>
+    (return $ foldr (\x y -> PCompound "." [x, y]) (PCompound "[]" []) lst)
 
 statementOrEnd :: (MonadState Environment m, Stream s m Char) =>
                   ParsecT s u m (Maybe Preterm)
 statementOrEnd =
-  Just <$> term False <* symbol "." <|>
+  Just <$> term [",", "|"] <* symbol "." <|>
   Nothing <$ eof
 
 -- program :: Stream s m Char => ParsecT s u m Program
---
+++ sample2.pl
@@ -0,0 +1,7 @@
+/* vim: set ft=prolog: */
+%append('[]',X,X).
+%append('.'(A,X),Y,'.'(A,Z)) :- append(X,Y,Z).
+%?- append('.'(x,'.'(y,'[]')), '.'(z,'[]'), X).
+append([],X,X).
+append([A|X],Y,[A|Z]) :- append(X,Y,Z).
+?- append([x,y], [z], X).
